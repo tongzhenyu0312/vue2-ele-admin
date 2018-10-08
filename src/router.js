8bc1ba9a1582653706293b0052b872f9b20a0a98
tongzhenyu <tongzhenyu@hellobike.com>
2018-10-08 15:25:13 +0800

    initial commit

--- src/router.js
+++ src/router.js
@@ -1,23 +1,19 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
-})
+      // 路由级别的代码分离,被访问的路由通过懒加载(生成一个独立的模块)的形式展现。
+      component: () =>
+        import('./views/Home.vue'),
+    },
+  ],
+});
